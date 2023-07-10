898b41347ab1bf9671b4728f4713d327708b98c0
devhongsa <bippr0901@gmail.com>
2023-07-10 20:12:15 +0900

    project setting : first commit

---
+++ src/main/java/com/devhong/free_coupon/FreeCouponApplication.java
@@ -0,0 +1,13 @@
+package com.devhong.free_coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeCouponApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devhong/free_coupon/FreeCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhong.free_coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
