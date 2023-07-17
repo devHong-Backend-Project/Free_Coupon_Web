ebc8dfafd9dd2f1bfbd705d8740f8aef3835a82e
devhongsa <bippr0901@gmail.com>
2023-07-18 01:35:52 +0900

    chore : Security,Jpa Configuration 설정
    passwordEncoder, JpaAuditing, Security 기본설정 추가

---
+++ src/main/java/com/devhong/free_coupon/security/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.devhong.free_coupon.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Slf4j
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true) //@PreAuthorize, @PostAuthorize를 쓸 수 있게 됨.
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    /*
+        client에서 요청을 날리면 Spring Security의 여러 filterChain 들을 거치게 됨.
+        그 체인들에 대한 설정을 하는 Bean 임.
+        disable()로 특정 필터를 거치지않게 할 수 있음.
+     */
+    private final JwtAuthenticationFilter authenticationFilter;
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**/signup", "/**/signin").permitAll()
+                .and()
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                //.antMatchers("/ignore1", "/ignore2")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+
+    }
+
+
+}
